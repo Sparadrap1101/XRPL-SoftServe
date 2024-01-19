4f0493587aa8c361e88b5e9a7f3684b8866c2806
Sparadrap1101 <alexis111@orange.fr>
2024-01-19 22:54:03 +0100

    Add 'App.js' frontend

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
+import React, { useState } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 
-const apiCallMint = () => {
-  axios.post("http://localhost:8080/mint").then((data) => {
-    console.log(data);
-  });
-};
-
-const apiCallCompare = () => {
-  axios.post("http://localhost:8080/compare").then((data) => {
-    console.log(data);
-  });
-};
-
 function App() {
+  const [file, setFile] = useState();
+  const [uploadText, setUploadText] = useState("Upload your Artwork here:");
+  const [image, setImage] = useState(null);
+  const [mintResult, setMintResult] = useState("");
+
+  const apiCallMint = () => {
+    axios.post("http://localhost:8080/mint").then((data) => {
+      console.log(data);
+    });
+  };
+
+  const apiCallCompare = () => {
+    axios.post("http://localhost:8080/compare", image).then((data) => {
+      console.log(data.data.result);
+
+      if (data.data.result == 0) {
+        setMintResult("Your NFT artwork's IP as been successfully minted!");
+        apiCallMint();
+      } else {
+        setMintResult("Unfortunately your artwork already have an IP owner.");
+      }
+    });
+  };
+
+  function handleChange(e) {
+    const formData = new FormData();
+    formData.append("my-image-file", e.target.files[0], e.target.files[0].name);
+
+    setImage(formData);
+    setUploadText("Here is your Artwork:");
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <button onClick={apiCallMint}>Mint</button>
-        <button onClick={apiCallCompare}>Compare</button>
+        <h1>SoftServe</h1>
+
+        <button className="button-85" onClick={apiCallCompare}>
+          <strong>Authenticate Your Artwork</strong>
+        </button>
+        <p>{mintResult}</p>
+
+        <div className="Image">
+          <h4>{uploadText}</h4>
+          <input type="file" id="file" className="inputfile" onChange={handleChange} />
+          <label for="file">
+            <u>Choose a file</u>
+          </label>
+        </div>
+        <div>
+          <img src={file} style={{ maxWidth: "200px", maxHeight: "200px" }} />
+        </div>
       </header>
     </div>
   );
